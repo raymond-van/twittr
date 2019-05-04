e47fc7a58dbbab4fb1400b77bbf8ccf1bbb8d4e8
raymond-van <vraymond028@gmail.com>
2019-05-03 22:41:19 -0400

    .

--- twittr/twittr/settings.py
+++ twittr/twittr/settings.py
@@ -132,26 +132,16 @@ USE_L10N = True
 USE_TZ = True
 
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_URL = '/feed/static/'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/feed/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
+
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'twittr/static'),
 )
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_CUSTOM_DOMAIN = os.environ.get('AWS_S3_CUSTOM_DOMAIN')
-AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400',}
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-AWS_LOCATION = 'static'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = 'https://twittr-profile-pics.s3.amazonaws.com/static/'
-
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# AWS_MEDIA_LOCATION = '/media/'
-DEFAULT_FILE_STORAGE = 'twittr.storage_backends.MediaStorage'  
 django_heroku.settings(locals())
